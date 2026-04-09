d330853c43e95cea5dafc0db6b6483bcae053da6
abevitor <blueprates86@gmail.com>
2026-04-09 11:56:54 -0300

    fazendo historico permanente

--- src/main/java/com/example/SpringBootAI/model/PromptLog.java
+++ src/main/java/com/example/SpringBootAI/model/PromptLog.java
@@ -19,8 +19,22 @@ public class PromptLog {
 
     @Column(columnDefinition = "TEXT")
     private String response;
+    
+    private String username;
+
+    
+
+    
 
   
+     public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
      public Long getId() {
         return id;
     }
--- src/main/java/com/example/SpringBootAI/repository/PromptRepository.java
+++ src/main/java/com/example/SpringBootAI/repository/PromptRepository.java
@@ -1,9 +1,13 @@
 package com.example.SpringBootAI.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.SpringBootAI.model.PromptLog;
 
 public interface PromptRepository extends JpaRepository<PromptLog, Long>{
+
+    List<PromptLog> findByUsername(String username);
     
 }
